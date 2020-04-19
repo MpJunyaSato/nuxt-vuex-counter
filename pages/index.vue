465b201a529fc9ff535fca812d31caccfc6af5e3
Junya Sato <sato@metaps-payment.com>
2020-04-19 17:02:07 +0900

    Section 9 カウンターアプリの開発

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Counter from "../components/Counter";
+  export default {
+    components: {
+      Counter
+    }
+  }
+</script>
